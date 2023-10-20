f594376869a8432f175ea84e3e1f4e9c8be76d51
Michael Steidel <masteidel@gmail.com>
2023-10-20 16:29:57 -0400

    Refactor pokecache for improved readability and efficiency

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -9,8 +9,8 @@ import (
 
 // Cache is a thread-safe map used to cache data.
 type Cache struct {
-	cache map[string]cacheEntry // Cache variable is a map which contains the cached data
-	mux   *sync.Mutex           // Mutex is used for handling access from multiple routines
+	cache map[string]cacheEntry // cache  is a map which contains the cached data
+	mux   *sync.Mutex           // mux is a Mutex which is used for handling access from multiple routines
 }
 
 // cacheEntry is a single entity/item stored in the cache
@@ -19,14 +19,19 @@ type cacheEntry struct {
 	val       []byte    // Value of the cache entry
 }
 
-// NewCache function creates a new cache.
+// NewCache function creates a new Cache.
 func NewCache(interval time.Duration) Cache {
+	// A Cache struct is being initialized with an empty cache and a new mutex.
 	c := Cache{
-		cache: make(map[string]cacheEntry), // initialize cache map
-		mux:   &sync.Mutex{},               // initialize mutex lock
+		cache: make(map[string]cacheEntry), // Creating an empty map of string keys to cacheEntry values.
+		mux:   &sync.Mutex{},               // Initializing a new mutex for handling concurrent access to the cache.
 	}
 
-	return c // return new cache
+	// Invoking the reapLoop method in a separate goroutine, which continually clears expired entries from the cache at the provided interval.
+	go c.reapLoop(interval)
+
+	// Returning the newly created cache.
+	return c
 }
 
 // Add method adds a new entry to the cache. It is thread-safe, i.e.,
@@ -49,22 +54,36 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	return val.val, ok // returns value and a boolean indicating if a value was found
 }
 
-// reapLoop is responsible for periodically checking and deleting expired cache entries
+// reapLoop is a method on Cache struct that runs a loop at specific intervals.
+// Each time the interval elapses, the method invokes the 'reap' method.
 func (c *Cache) reapLoop(interval time.Duration) {
+	// Create a new ticker that triggers at intervals based on the specified duration.
 	ticker := time.NewTicker(interval)
+
+	// The loop will continuously wait for the ticker's channel to send a signal
+	// It triggers once the time interval specified when creating the ticker elapses
 	for range ticker.C {
-		c.reap(time.Now().UTC(), interval) // call reap method to remove expired entries
+		// After each trigger, call the reap method on the Cache. Pass the current time and the interval.
+		c.reap(time.Now().UTC(), interval)
 	}
 }
 
-// reap method checks for expired cache entries and deletes them. This method
-// must be safely used in a concurrent context.
+// `reap` is a method on `Cache` struct which clears items from the cache that were created
+// before a specified time duration (`last`). The `now` parameter represents the current time.
 func (c *Cache) reap(now time.Time, last time.Duration) {
-	c.mux.Lock()         // Lock before modifying the cache
-	defer c.mux.Unlock() // Unlock after modification is complete
+	// Locking the mutex to prevent concurrent read/write on the map `c.cache`
+	c.mux.Lock()
+
+	// Unlocking the mutex once this function finishes its execution. This is done
+	// using the `defer` statement which ensures that the lock is released
+	// even if an error occurs during the execution of this function.
+	defer c.mux.Unlock()
+
+	// Iterating over each key-value pair in the cache
 	for k, v := range c.cache {
+		// Deleting all the cache entries that were created before the specified duration
 		if v.createdAt.Before(now.Add(-last)) {
-			delete(c.cache, k) // delete expired entries
+			delete(c.cache, k)
 		}
 	}
 }
